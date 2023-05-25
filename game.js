31c36cab43d6f03e1b7f95fa87fd897e0be12ddb
H-Vasq <123214691+H-Vasq@users.noreply.github.com>
2023-05-24 20:18:00 -0600

    Fix error game js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementByClassName('choice-text'));
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 
 let currentQuestion = {};
